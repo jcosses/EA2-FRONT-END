a89ebf3fb8039540637f215fb0520943dfaaad86
jcosses <jua.ossesv@profesor.duoc.cl>
2023-01-13 16:30:11 -0300

    Jquery 13-01

---
+++ js/main-jquery.js
@@ -0,0 +1,62 @@
+// Cambiar contenido de Etiqueta h1
+    // Con JavaScript Puro
+    // console.log("Pagina Funcionando")
+    //document.getElementById('Titulo').innerHTML = "TITULO DE PAGINA";
+   
+
+// Incorporar Libreria JQuery
+
+$(document).ready(function(){
+    /*
+    
+    console.log("Pagina Funcionando")
+    
+    // SELECCIONAR ELEMENTOS HTML
+    $('h1').html('Texto modificado por Etiqueta');    
+    $('#Titulo').html('Texto modificado por su ID de Etiqueta');
+    $('.display-4').html('Texto modificado por su CLASE de Etiqueta');
+    $('#p2').html('Texto Modificado desde jquery');
+    $('.display-5 #p3').html('Parrafo 3 Modificado');
+    
+
+    // AGREGAR CLASE  [ jq.classAdd ]
+    $('h1').addClass('text-center text-uppercase');
+    $('#p2').addClass('text-danger');
+
+    // QUITAR CLASE  [ jq.classRemove ]
+    $('#Parrafos #p3').removeClass('display-6');
+
+    // AGREGAR ELEMENTO [ jq.Append ]
+    $('#Parrafos').append('<p>Cuarto Parrafo agregado desde jquery..</p>');
+
+    // QUITAR ELEMENTO [ jq.Remove ]
+    $('#p2').remove();
+    $('#p2').hide();
+
+
+    // MODIFICAR PROPIEDADES CSS [ jqCssSet ]
+    $('#p1').css('color', 'blue');
+    $('#p3').css({color: 'blue', background:'green', padding:'20px'});
+
+    // METODO attr()      [ jqAttrSet]
+    $('img').attr('src', 'https://www.sabbagharquitectos.com/wp-content/thumbgen_cache/5b086a1c261dff9e5d9dc85f0e6caf01.jpg');
+    $('img').attr('width','300');
+
+    // EVENTOS CON JQUERY
+    
+    //Evento click Agregando clase
+    $('#btn1').click(function(){
+        $('#contenido').addClass('text-center');
+    });
+
+    //Evento click Agregando estilo en css
+    $('#btn2').click(function(){
+        $('#contenido h1').css('color','salmon');
+    });
+
+    //Alternativa al evento click
+    $('#btn3').on('click',function(){
+        $('#contenido h1').toggleClass('display-4');
+    });
+ */
+})
---
+++ js/mainjquery.js
@@ -0,0 +1,33 @@
+// Cambiar contenido de Etiqueta h1
+    // Con JavaScript Puro
+    // console.log("Pagina Funcionando")
+    //document.getElementById('Titulo').innerHTML = "TITULO DE PAGINA";
+   
+
+// Incorporar Libreria JQuery
+
+$(document).ready(function(){
+    console.log("Pagina Funcionando")
+    
+    // SELECCIONAR ELEMENTOS HTML
+    
+    // AGREGAR CLASE  [ jq.classAdd ]
+    
+    // QUITAR CLASE  [ jq.classRemove ]
+    
+    // AGREGAR ELEMENTO [ jq.Append ]
+    
+    // QUITAR ELEMENTO [ jq.Remove ]
+   
+    // MODIFICAR PROPIEDADES CSS [ jqCssSet ]
+    
+    // METODO attr()      [ jqAttrSet]
+    
+    // EVENTOS CON JQUERY
+    //Evento click Agregando clase
+    
+    //Evento click Agregando estilo en css
+    
+    //Alternativa al evento click
+
+})
